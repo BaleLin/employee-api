24d80afa76773e3d15d178c2f5abbb52d459392a
BaleLin <bale.lin@oocl.com>
2018-07-24 16:59:47 +0800

    补上结果图

--- src/main/java/com/oocl/employeeapi/service/EmployeeService.java
+++ src/main/java/com/oocl/employeeapi/service/EmployeeService.java
@@ -33,8 +33,7 @@ public class EmployeeService {
     public List<Employees> updateEmployee(int id,Employees employees){
         for(int i=0;i<all.size();i++){
             if (all.get(i).getId()==id){
-                all.remove(new Employees(id));
-                all.add(employees);
+                all.set(i,new Employees(id,employees.getName(),employees.getAge(),employees.getGender()));
             }
         }
         return all;
